d9c1fc332723b0729256e07a2c346e00c8d64701
Andrei Cataraga <andreicataraga18@gmail.com>
2024-03-02 23:26:20 +0100

    Update REST Client

--- src/main/java/com/zaurtregulov/spring/rest/App.java
+++ src/main/java/com/zaurtregulov/spring/rest/App.java
@@ -1,13 +1,32 @@
 package com.zaurtregulov.spring.rest;
 
+import com.zaurtregulov.spring.rest.configuration.MyConfig;
+import com.zaurtregulov.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main( String[] args ) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication",
+                Communication.class);
+
+//        List<Employee> allEmployees = communication.getAllEmployees();
+//        System.out.println(allEmployees);
+
+//        Employee empByID = communication.getEmployee(1);
+//        System.out.println(empByID);
+
+//        Employee emp = new Employee("Valeriu", "Plop", "IT", 950);
+//        emp.setId(11);
+//        communication.saveEmployee(emp);
+
+        communication.deleteEmployee(10);
     }
 }
